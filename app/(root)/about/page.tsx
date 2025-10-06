b101d869ce94a3b4be66f72007a3c77b805ffa4b
ahmedfadl3019@gmail.com <ahmedfadl3019@gmail.com>
2025-10-06 16:24:27 +0200

    Create error files on all levels and notice which one will display ?

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import About from "../../components/about";
 
 export default function AboutPage() {
+  throw new Error("This is a simulated error from about page");
   return <About />;
 }
